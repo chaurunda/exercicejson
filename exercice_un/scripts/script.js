037a50472f252c6d4b7146c078b51fe9974717fb
arnaud-huc <arnaud-huc@cifacom.com>
2015-07-20 13:34:50 +0200

    Correction orthographe

--- exercice_un/scripts/script.js
+++ exercice_un/scripts/script.js
@@ -1,8 +1,8 @@
 /*
  * Instruction :
  * 1 : Faire en sorte que notre script commence une fois le DOM chargé
- * 2 : Effectuer une requête AJAX qui va appelé le fichier index.json
- * 3 : Dans le success de notre requete, insérer la réponse dans notre HTML après le header
+ * 2 : Effectuer une requête AJAX qui va appeler le fichier index.json
+ * 3 : Dans le success de notre requête, insérer la réponse dans notre HTML après le header
  * ---------------
  *
  * N'oubliez pas de regarder la documentation que j'ai écrite. N'hésitez pas non plus à aller faire
