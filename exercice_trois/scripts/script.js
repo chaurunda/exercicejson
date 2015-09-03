b671347f452be754323e02f1a6a245ab09474441
arnaud-huc <arnaud-huc@cifacom.com>
2015-09-03 10:57:47 +0200

    Correct errors, change text ...

--- exercice_trois/scripts/script.js
+++ exercice_trois/scripts/script.js
@@ -1,23 +1,23 @@
 /*
- * Instruction :
+ * Instructions :
  * 1 : Faire en sorte que notre script commence une fois le DOM chargé
  * 2 : Effectuer une requête AJAX qui va appeler le fichier index.json
- * 3 : Dans le success de notre requete, faite une boucle sur votre résultat afin de pour l'afficher
+ * 3 : Dans le success de notre requête, faites une boucle sur votre résultat afin de pouvoir l'afficher
  *     ligne par ligne (Voir l'aide plus bas).
- * 4 : Insérer les résultats formaté dans la liste '.list-group'.
+ * 4 : Insérer les résultats formatés dans la liste '.list-group'.
  * 5 : Allez à l'adresse suivante : https://developers.google.com/maps/documentation/javascript/examples/map-simple
  * 6 : Adaptez le code pour que, quand vous récupérez le fichier json, la fonction initialize s'éxecute.
- * 7 : Allez à l'address suivante : https://developers.google.com/maps/documentation/javascript/examples/marker-simple
- * 8 : Faites en sorte de boucler sur votre résultat afin d'afficher les différents marker sur la map (situé à Berlin);
+ * 7 : Allez à l'addresse suivante : https://developers.google.com/maps/documentation/javascript/examples/marker-simple
+ * 8 : Faites en sorte de boucler sur votre résultat afin d'afficher les différents markers sur la map (situé à Berlin);
  * BONUS :
- * Centrez la map parmis les différents marker afin qu'elle soit centrée sur tout les markers.
+ * Centrez la map parmis les différents markers afin qu'elle soit centrée sur tout les markers.
  * ---------------
  * AIDE :
- * Voici à quoi devrait ressembler ce que vous insérer dans le DOM :
+ * Voici à quoi devrait ressembler ce que vous insérez dans le DOM :
  * '<li class="list-group-item"><span class="name">Nom du magasin</span><span class="address">Adresse du magsin</span><span class="postalCode">Code postal du magasin</span>';
  *
  *
  * N'oubliez pas de regarder la documentation que j'ai écrite. N'hésitez pas non plus à aller faire
  * un tour sur la doc de jQuery.
- * -- Fin du premier exercice --
+ * -- Fin du troisième exercice --
  */
